e5cb7c9c734e01f651525d4f5cb09b95485b6ba4
Mayank Kumar <mayank12999@gmail.com>
2025-06-30 18:24:54 +0530

    Update source-code.c

--- source-code.c
+++ source-code.c
@@ -1,18 +1,14 @@
-// C program to build the outline 
-// boundary using draw() 
 #include <stdio.h> 
 #include <stdlib.h> 
+
 int i, j, height = 30; 
 int width = 30, gameover, score; 
 
-// Function to draw a boundary 
 void draw() 
 { 
-	// system("cls"); 
 	for (i = 0; i < height; i++) { 
 		for (j = 0; j < width; j++) { 
-			if (i == 0 || i == width - 1 || j == 0 
-				|| j == height - 1) { 
+			if (i == 0 || i == width - 1 || j == 0 || j == height - 1) { 
 				printf("#"); 
 			} 
 			else { 
@@ -23,11 +19,8 @@ void draw()
 	} 
 } 
 
-// Driver Code 
 int main() 
 { 
-	// Function Call 
 	draw(); 
-
 	return 0; 
 }
